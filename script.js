4879e3a34998b36eeaff8ab2e58292382176824a
Dempsey FP <dempseyfinley@gmail.com>
2020-10-03 17:13:02 -0400

    for loop, question array, html divs created

---
+++ script.js
@@ -0,0 +1,34 @@
+var questionsSet = [
+    {question:"What is number one?",
+    answer_choices:["one","two","three","four"],
+    answer:"one",
+    },
+    {question:"What is number one?",
+    answer_choices:["one","two","three","four"],
+    answer:"one",
+    },
+    {question:"What is number one?",
+    answer_choices:["one","two","three","four"],
+    answer:"one",
+    },
+    {question:"What is number one?",
+    answer_choices:["one","two","three","four"],
+    answer:"one",
+    },
+    {question:"What is number one?",
+    answer_choices:["one","two","three","four"],
+    answer:"one",
+    },
+    {question:"What is number one?",
+    answer_choices:["one","two","three","four"],
+    answer:"one",
+    }
+]
+console.log(questionsSet.length);
+console.log(questionsSet[0].answer)
+// function quizStart(){
+
+// }
+for (i =0; i < questionsSet.length;i++){
+    console.log(questionsSet[i])
+}
